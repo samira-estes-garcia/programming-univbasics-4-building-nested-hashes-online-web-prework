fca40b53081ea732472f88336564374dfa82a51c
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-03 15:08:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => 
 	    {
-	      :key1 => "",
+	      :one_piece_owned => "",
 	      :key2 => "",
 	      :key3 => "",
 	      :key4 => ""
