0ac708a40b454ef2620df3be2569661001e60338
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-03 15:10:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
 	    :names => 
 	    {
 	      :reading_railroad => {},
-	      :name2 => "",
+	      :pennsylvania_railroad => {},
 	      :name3 => "",
 	      :name4 => ""
 	    }
