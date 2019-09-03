6d5a9a5be69a9945b0db9963f0681cc8524047df
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-03 15:14:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -76,7 +76,10 @@ def monopoly_with_fourth_tier
 	      {
 	        "mortgage_value" => "$400"
 	      },
-	      :shortline_railroad => {}
+	      :shortline_railroad => 
+	      {
+	        "mortgage_value" => "$800"
+	      }
 	    }
 	  }
 	}
